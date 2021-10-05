a86d97128680cf7f9089020b2b49174897e242c0
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-10-05 17:59:07 +0200

    iteration3

--- iteration3.js
+++ iteration3.js
@@ -1,4 +1,12 @@
 //iteration 3
 
-const numbers = [12, 21, 38, 5, 45, 37, 6];
-function average(param) 
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+function sumAll(array){
+    let result = array[0]
+    for (let index = 0; index < array.length; index++) {
+        result += array[index]
+    }
+    console.log(result)
+}
+
+sumAll(numbers)
