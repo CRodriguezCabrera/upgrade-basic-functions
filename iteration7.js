9d8e07863548145a8f74271b58deae64fba5bc79
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-10-06 17:16:12 +0200

    iteration7

---
+++ iteration7.js
@@ -0,0 +1,33 @@
+/* **Iteración #7: Buscador de nombres**
+
+Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba 
+si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. 
+Puedes usar este array para probar tu función:
+
+ */
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function finderName(array, name) {
+    
+    for (let index = 0; index < array.length; index++) {
+        if(array[index] == name){
+            console.log(true, "en la posicion " + [index])
+        }else{
+            console.log(false)
+        }    
+        
+    }
+   
+}
+finderName (nameFinder, "Bruce")
