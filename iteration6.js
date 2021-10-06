3bc208d23c008713d4bc700f36f022fa43436121
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-10-06 16:54:25 +0200

    iteration6

---
+++ iteration6.js
@@ -0,0 +1,30 @@
+/* /Iteración #6: Valores únicos**
+
+Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los
+ elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
+
+ */
+const duplicates = [
+  'sushi',
+  'pizza',
+  'burger',
+  'potatoe',
+  'pasta',
+  'ice-cream',
+  'pizza',
+  'chicken',
+  'onion rings',
+  'pasta',
+  'soda'
+];
+function removeDuplicates(array) {
+    let noDuplicates = [];
+    for (let index = 0; index < array.length; index++) {
+        if(!noDuplicates.includes(array[index])){
+            noDuplicates.push(array[index]);
+        }
+        
+    }
+    return console.log(noDuplicates)
+}
+removeDuplicates(duplicates)
